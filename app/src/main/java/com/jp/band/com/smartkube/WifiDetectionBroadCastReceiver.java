889095500a64c0470b18ff19e8fbdca2614c5899
kai <pavanvenkatesh94@gmail.com>
2016-04-06 10:27:20 +0530

    first commit

---
+++ app/src/main/java/com/jp/band/com/smartkube/WifiDetectionBroadCastReceiver.java
@@ -0,0 +1,17 @@
+package com.jp.band.com.smartkube;
+
+/**
+ * Created by kai on 5/4/16.
+ */
+
+        import android.content.BroadcastReceiver;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.widget.Toast;
+
+public class WifiDetectionBroadCastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
